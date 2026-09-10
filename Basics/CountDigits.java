fe04d0beeb0e7e6c1f1fca681876694e8275ce2c
misbahfathima786 <misbahfathima785@gmail.com>
2026-09-10 06:59:12 +0530

    Update CountDigits.java

--- Basics/CountDigits.java
+++ Basics/CountDigits.java
@@ -1,21 +1,27 @@
 /*
-Problem : Count Digits.
-Platform : Online Compiler.
-problem number : 1
+Problem 1 : Count Digits.
+Platform : Online Compiler
 */
 
 import java.util.*;
 public class CountDigits {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Enter a number : ");
-      int n = sc.nextInt();
+    public static int countDigits(int n) {
       int count = 0;
+      if(n==0) {
+        return count+1;
+      }
       while(n!=0) {
         count++;
         n/=10; //To remove last digit
       }
-      System.out.println("Number of digits in a given number = "+count);
+      return count;
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a number : ");
+      int n = sc.nextInt();
+      int result = countDigits(n);
+      System.out.println("Number of digits in a given number = "+result);
       sc.close();
     }
 }
