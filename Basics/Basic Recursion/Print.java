dc309b35e7f13b2d8e37e201b4d056dace345f6a
misbahfathima786 <misbahfathima785@gmail.com>
2026-09-10 16:05:01 +0530

    Update Print.java

--- Basics/Basic Recursion/Print.java
+++ Basics/Basic Recursion/Print.java
@@ -1,1 +1,21 @@
+/*
+Simple problem to start or get the idea about recursion
+print the name recursively till it reaches the base case
+*/
 
+import java.util.*;
+public class Recursion {
+    public static int rec(int n) {
+      if(n==0) {
+        return 0;
+      }
+      else {
+        System.out.println("Misbah");
+        return rec(n-1);
+      }
+    }
+    public static void main(String[] args) {
+      int n = 3;
+      rec(n);
+    }
+}
