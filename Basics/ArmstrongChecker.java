0b363b6f4595d17fa376959a2c5b5ae32a71e844
misbahfathima786 <misbahfathima785@gmail.com>
2026-09-10 12:06:05 +0530

    Create ArmstrongChecker.java

---
+++ Basics/ArmstrongChecker.java
@@ -0,0 +1,37 @@
+/*
+Problem 5: Armstrong Numbers.
+Platform: Online Compiler.
+
+Complexity Analysis
+● Time Complexity: O(log₁₀ n)
+● Space Complexity: O(1)
+*/
+
+public class ArmstrongChecker {
+
+    // Static method to check if a number is an Armstrong number
+    public static boolean isArmstrong(int num) {
+        int k = String.valueOf(num).length(); // Get number of digits
+        int sum = 0;
+        int n = num;
+
+        while (n > 0) {
+            int ld = n % 10;             // Last digit
+            sum += Math.pow(ld, k);      // Add ld^k
+            n /= 10;                     // Remove digit
+        }
+
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        int number = 153;
+
+        // Use class method to check
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+}
