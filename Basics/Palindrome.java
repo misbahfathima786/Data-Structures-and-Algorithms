8e1fd88072218d41a6e8f3990976b2b2f64a558a
misbahfathima786 <misbahfathima785@gmail.com>
2026-09-10 07:13:52 +0530

    Create Palindrome.java

---
+++ Basics/Palindrome.java
@@ -0,0 +1,34 @@
+/*
+Problem 3 : Reverse a Number.
+Platform : Online Compiler.
+
+Complexity Analysis
+● Time Complexity: O(log₁₀ n)
+● Space Complexity: O(1)
+*/
+
+import java.util.*;
+public class Palindrome {
+    public static int palindrome(int n) {
+      int rev = 0;
+      while(n!=0) {
+        int digit = n%10; //To extract last digit
+        rev = rev * 10 + digit;//To add the extracted number at first(To reverse).
+        n/=10; //To remove last digit
+      }
+      return rev;
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a number : ");
+      int n = sc.nextInt();
+      int reverse = palindrome(n);
+      if(n==reverse) {
+        System.out.println("Given number is Palindrome.");
+      }
+      else {
+        System.out.println("Given number is not a Palindrome.");
+      }
+      sc.close();
+    }
+}
