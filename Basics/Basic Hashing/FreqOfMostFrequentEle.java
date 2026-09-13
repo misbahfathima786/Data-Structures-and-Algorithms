5938829b6240a2ed4303659c8c5af54388a27bf1
misbahfathima786 <misbahfathima785@gmail.com>
2026-09-13 23:32:09 +0530

    Update FreqOfMostFrequentEle.java

--- Basics/Basic Hashing/FreqOfMostFrequentEle.java
+++ Basics/Basic Hashing/FreqOfMostFrequentEle.java
@@ -9,21 +9,23 @@ Complexity Analysis
 
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        int left = 0 , maxFreq = 0 ;
-        long sum = 0;
-        Arrays.sort(nums);
+        int left = 0 , maxFreq = 0 ; //left for sliding window concept , maxFreq to update frequency of an element in an array and to return result.
+        long sum = 0; // to calculate sum of the elements present in the current window.
+        Arrays.sort(nums); // to do sliding window .
+        
         for(int right = 0; right<nums.length; right++) {
-            int ws = right - left + 1;
-            sum += nums[right];
-            long operations = (long) nums[right]*ws - sum;
-            while(operations>k) {
-                left++;
-                ws = right-left+1;
-                sum -= nums[left-1];
-                operations = (long) nums[right]*ws - sum;
+            int ws = right - left + 1; // windpw size
+            sum += nums[right]; // sum calculation
+            long operations = (long) nums[right]*ws - sum; // to find how many operation we need to make other elements == target (nums[right])
+            
+            while(operations>k) { // we can not make other elements == target so move window.
+                left++; 
+                ws = right-left+1; 
+                sum -= nums[left-1]; // as we move one step forward , we have to update our sum also.
+                operations = (long) nums[right]*ws - sum; // as we move one step forward , we have to update our operations also.
             }
             if(ws > maxFreq) {
-                maxFreq = ws;
+                maxFreq = ws; // if operations<=k , means we can add +1 to other elements to make equal to target . 
             }
         }
         return maxFreq;
